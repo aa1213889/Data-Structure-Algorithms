881a8585169160f11303608e60f05086a6a78512
aa1213889 <869135421@qq.com>
2021-12-22 22:26:40 +0800

    change md

--- test.js
+++
@@ -1,9 +0,0 @@
-const popUp = {}
-popUp[Symbol('isOpen')] = true
-popUp[Symbol('isOpen')] = 123
-console.log(popUp)
-console.log(Object.getOwnPropertySymbols(popUp))
-
-const objVals = Object.getOwnPropertySymbols(popUp)
-console.log(popUp[objVals[0]]) //true
-console.log(popUp[objVals[1]]) //123
